7f7c30dacc163425537fd53ddffb46b43e0e2e9d
Arlen Pan <arlenpan@gmail.com>
2022-03-29 18:22:20 -0700

    add hotjar

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,8 +1,17 @@
 import 'antd/dist/antd.css';
 import 'src/styles/globals.scss';
 import 'src/styles/utility.scss';
+import { hotjar } from 'react-hotjar';
+import { useEffect } from 'react';
+
+const HJID = 2900319;
+const HJSV = 6;
 
 function MyApp({ Component, pageProps }) {
+    useEffect(() => {
+        hotjar.initialize(HJID, HJSV);
+    }, []);
+
     // Use the layout defined at the page level, if available
     const getLayout = Component.getLayout || ((page) => page);
     return getLayout(<Component {...pageProps} />);
