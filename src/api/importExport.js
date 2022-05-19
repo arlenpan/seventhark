4b0960aa7e134cbb3b81dbb3a1055a22822eb673
Arlen Pan <arlenpan@gmail.com>
2022-05-19 01:19:14 -0700

    add more keys to import export

--- src/api/importExport.js
+++ src/api/importExport.js
@@ -1,12 +1,21 @@
 import { CHARACTER_KEY } from './character';
-import { CHECKLIST_KEY } from './checklist';
+import { CHECKLIST_CUSTOM_EVENTS, CHECKLIST_HIDDEN_EVENTS, CHECKLIST_KEY } from './checklist';
 import { COSTS_KEY, TIERS_KEY } from './economy';
 import { ISLAND_KEY } from './islands';
 import { getLocal, setLocal } from './localStorage';
 import { RAPPORT_KEY } from './rapport';
 
 const FILENAME = 'seventharkdata';
-const STORAGE_KEYS = [CHARACTER_KEY, CHECKLIST_KEY, TIERS_KEY, COSTS_KEY, ISLAND_KEY, RAPPORT_KEY];
+const STORAGE_KEYS = [
+    CHARACTER_KEY,
+    CHECKLIST_KEY,
+    CHECKLIST_CUSTOM_EVENTS,
+    CHECKLIST_HIDDEN_EVENTS,
+    TIERS_KEY,
+    COSTS_KEY,
+    ISLAND_KEY,
+    RAPPORT_KEY,
+];
 
 const downloadObjectAsJson = (exportObj, exportName) => {
     const dataStr = `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(exportObj))}`;
